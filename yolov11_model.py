a033090343bc63f76f674cc7346ab69ebb1cf352
Md. Sadman Haque <137508387+shadib007@users.noreply.github.com>
2025-07-02 22:43:08 +0600

    Update yolov11_model.py

--- yolov11_model.py
+++ yolov11_model.py
@@ -1,10 +1,14 @@
 from ultralytics import YOLO
+import numpy as np
 import cv2
+from PIL import Image
 
-# Load your model
-model = YOLO("best.pt")
+model = YOLO('best.pt')
 
-def detect_frame(frame):
-    results = model(frame)
-    annotated = results[0].plot()
-    return annotated
+def detect_image(pil_img):
+    img = np.array(pil_img)
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    results = model(img)
+    rendered = results[0].plot()
+    rendered = cv2.cvtColor(rendered, cv2.COLOR_BGR2RGB)
+    return Image.fromarray(rendered)
